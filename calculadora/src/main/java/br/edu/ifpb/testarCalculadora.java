04609975c933703ac530f60d4930b8ce4b12c808
gasperpb <achillespb@hotmail.com>
2021-03-28 21:47:26 -0300

    :UP:

---
+++ calculadora/src/main/java/br/edu/ifpb/testarCalculadora.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb;
+
+import java.util.Scanner;
+
+public class testarCalculadora {
+    public static void main(String[] args) {
+
+        AppCalculadora obj1 = new AppCalculadora();
+        obj1.calcular(0);
+
+    }
+
+}
